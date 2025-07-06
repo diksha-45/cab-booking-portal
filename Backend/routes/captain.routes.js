b13164b199c8f78c0a290d935359470df900edf8
Karan8890 <karansinghranawat17@gmail.com>
2025-07-06 20:30:34 +0530

    Added frontend files

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -15,7 +15,17 @@ router.post('/register', [
     body('vehicle.vehicleType').isIn([ 'car', 'motorcycle', 'auto' ]).withMessage('Invalid vehicle type')
 ], captainController.registerCaptain)
 
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+    captainController.loginCaptain
+)
 
+
+router.get('/profile', authMiddleware.authCaptain, captainController.getCaptainProfile)
+
+router.get('/logout', authMiddleware.authCaptain, captainController.logoutCaptain)
 
 
 module.exports = router;
